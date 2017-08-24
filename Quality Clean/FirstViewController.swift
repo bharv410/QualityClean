ef98297dc956389acc872a265463075f69284a2b
benharvey <bharvey@tigertext.com>
2017-08-24 08:42:01 -0700

    Initial Commit

---
+++ Quality Clean/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Quality Clean
+//
+//  Created by Ben Harvey on 8/24/17.
+//  Copyright © 2017 Quality Clean Nation LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
