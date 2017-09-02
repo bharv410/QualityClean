5614e59ee07862369fd5e24f4595a289cfecb3bc
benharvey <bharvey@tigertext.com>
2017-09-01 18:51:05 -0700

    good updates

--- Quality Clean/FirstViewController.swift
+++ Quality Clean/FirstViewController.swift
@@ -14,14 +14,14 @@ import AVKit
 import AVFoundation
 
 class FirstViewController: UIViewController {
-
+    
     @IBOutlet weak var textField: UITextField!
     @IBOutlet var user = Auth.auth().currentUser
     @IBOutlet weak var jobsLabel: UILabel!
     @IBOutlet weak var vidViewHolder: UIView!
     
     @IBOutlet weak var callUsNowButton: UIButton!
-
+    
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var bedBathLabel: UILabel!
     
@@ -37,174 +37,193 @@ class FirstViewController: UIViewController {
     
     @IBOutlet weak var profileImageView: UIImageView!
     
+    @IBOutlet weak var scrollView: UIScrollView!
     var ref: DatabaseReference!
     var player: AVPlayer!
     var avpController = AVPlayerViewController()
     
-    var isCleaner = false;
+    var isCleaner = false
+    var completedSignup = false
+    var hasVid = false
+    
+    var originalScrollSize = CGSize()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         ref = Database.database().reference()
+        originalScrollSize = scrollView.contentSize
         fetchUserData()
-}
+    }
     
     func fetchUserData(){
-        var hasBedBath = false
-
         let currentUserRef = ref.child("users").queryOrdered(byChild: "email")
             .queryEqual(toValue: user?.email).observeSingleEvent(of: .value, with: { (snapshot) in
                 if let value = snapshot.value as? NSDictionary{
                     let thisUserObject = value.allValues.first as! NSDictionary
-
-                    
-                    if let fullName = thisUserObject["bed_bath"] {
-                        print(fullName)
-                        hasBedBath = true
-                        
+                    if let userType = thisUserObject["user_type"]{
+                        let userTypeString = userType as! String
+                        if((userTypeString == "customer")){
+                            self.completedSignup = true
+                            self.isCleaner = false
+                        }
+                        if(userTypeString == "cleaner"){
+                            self.completedSignup = true
+                            self.isCleaner = true
+                        }
                     }
+
+                    if let bedBath = thisUserObject["bed_bath"]{
+                        self.bedBathLabel.text = bedBath as! String
+                    }
+                    
+                    if let email = thisUserObject["email"]{
+                        self.emailLabel.text = email as! String
+                    }
+                    
+                    if let address = thisUserObject["address"]{
+                        self.addressLabel.text = address as! String
+                    }
+                    
+                    if let fullName = thisUserObject["full_name"]{
+                        self.jobsLabel.text = fullName as! String
+                    }
+                    
+                    if let phoneNum = thisUserObject["phone_number"]{
+                        self.phoneNumberLabel.text = phoneNum as! String
+                    }
+                    
+                    if let address = thisUserObject["home_type"]{
+                        self.homeTypeLabel.text = address as! String
+                    }
+                    
+                    if(self.isCleaner){
+                    if let video = thisUserObject["video"]{
+                        self.hasVid = true
+                        let url = URL(string:video as! String)
+                        self.player = AVPlayer(url: url!)
+                        self.avpController = AVPlayerViewController()
+                        self.avpController.player = self.player
+                        self.avpController.view.frame = self.vidViewHolder.frame
+                        self.addChildViewController(self.avpController)
+                        self.scrollViewContentView.addSubview(self.avpController.view)
+                        self.scrollView.contentSize = self.originalScrollSize
+
+                        }
+                    }
+                    
+                    if let image = thisUserObject["image"]{
+                        let url = URL(string:image as! String)
+                        self.profileImageView.downloadedFrom(url: url!)
+                    }
+                }else{
+                    self.logoutUser()
                 }
             }
         )
-        if(!hasBedBath){
-            print("nil")
-            let vc = self.storyboard?.instantiateViewController(withIdentifier: "CompleteSignUp")
-            self.present(vc!, animated: true, completion: nil)
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(5), execute: {
+            self.callBackAfterLoginDone()
+        }
+        )
+    }
+    
+    
+    func callBackAfterLoginDone(){
+        if(!self.completedSignup){
+            self.finishSignUp()
+            
+            if(self.isCleaner){
+                if  let arrayOfTabBarItems = self.tabBarController?.tabBar.items as! AnyObject as? NSArray,let tabBarItem = arrayOfTabBarItems[1] as? UITabBarItem {
+                    tabBarItem.isEnabled = false
+                }
+            }
+            
+            
+        }
+        
+        if(!self.hasVid){
+            self.vidViewHolder.isHidden = true
+            scrollView.contentSize = CGSize(width: scrollView.contentSize.width,height: 1.0)
+
         }
     }
-
-//    func fetchUserData(){
-//        let currentUserRef = ref.child("users").queryOrdered(byChild: "email")
-//            .queryEqual(toValue: user?.email).observeSingleEvent(of: .value, with: { (snapshot) in
-//                
-//                if let value = snapshot.value as? NSDictionary{
-//                    var hasVid = false
-//                    //GOT USER OBJECT
-//                    let thisUserObject = value.allValues.first as! NSDictionary
-//                    
-//                    
-//                    //let fullName = thisUserObject.value(forKey: <#T##String#>)
-//                    
-//                    //if(value.has)
-//                    if let fullName = thisUserObject["full_name"] {
-//                        
-//                        
-//                    }else{
-//                        let vc = self.storyboard?.instantiateViewController(withIdentifier: "CompleteSignUp")
-//                        self.present(vc!, animated: true, completion: nil)
-//                    }
-//                    
-//                    for actualProperties in thisUserObject as NSDictionary{
-//                        if let keyTitle = actualProperties.key as? String {
-//                            
-//                            if keyTitle == "full_name"{
-//                                if let text = actualProperties.value as? String {
-//                                    self.jobsLabel.text = text
-//                                }
-//                            }
-//                            var customer = false
-//                            
-//                            if keyTitle == "user_type"{
-//                                if let text = actualProperties.value as? String {
-//                                    if (text == "customer"){
-//                                        customer = true
-//                                        self.isCleaner = false
-//                                        print("customer")
-//                                    }else{
-//                                        print("cleaner")
-//                                    }
-//                                }
-//                            }
-//                            
-//                            
-//                            
-//                            if keyTitle == "bed_bath"{
-//                                if let text = actualProperties.value as? String {
-//                                    self.bedBathLabel.text = text
-//                            }
-//                            
-//                            if keyTitle == "email"{
-//                                if let text = actualProperties.value as? String {
-//                                    self.emailLabel.text = text
-//                                }
-//                            }
-//                            
-//                            if keyTitle == "address"{
-//                                if let text = actualProperties.value as? String {
-//                                    self.addressLabel.text = text
-//                                }
-//                            }
-//                            
-//                            if keyTitle == "home_type"{
-//                                if let text = actualProperties.value as? String {
-//                                    self.homeTypeLabel.text = text
-//                                }
-//                            }
-//                            
-//                            if keyTitle == "phone_number"{
-//                                if let text = actualProperties.value as? String {
-//                                    self.phoneNumberLabel.text = text
-//                                }
-//                            }
-//                            
-//                            
-//                            if keyTitle == "full_name"{
-//                                if let text = actualProperties.value as? String {
-//                                    self.jobsLabel.text = text
-//                                }
-//                            }
-//                            
-//                            
-//                            
-//                            
-//                            if keyTitle == "video"{
-//                                hasVid = true
-//                                let url = URL(string:actualProperties.value as! String)
-//                                self.player = AVPlayer(url: url!)
-//                                self.avpController = AVPlayerViewController()
-//                                self.avpController.player = self.player
-//                                self.avpController.view.frame = self.vidViewHolder.frame
-//                                self.addChildViewController(self.avpController)
-//                                self.scrollViewContentView.addSubview(self.avpController.view)
-//                                self.isCleaner = true
-//                            }
-//                            
-//                            if keyTitle == "image"{
-//                                let url = URL(string:actualProperties.value as! String)
-//                                
-//                                self.profileImageView.downloadedFrom(url: url!)
-//                                }
-//                            }
-//                        }
-//                    }
-//                    
-//                    
-//                    if(self.isCleaner){
-//                        if  let arrayOfTabBarItems = self.tabBarController?.tabBar.items as! AnyObject as? NSArray,let tabBarItem = arrayOfTabBarItems[1] as? UITabBarItem {
-//                            tabBarItem.isEnabled = false
-//                        }
-//                    }else{
-//                        self.vidViewHolder.isHidden = true
-//                    }
-//                    
-//                }else{
-//                    self.logoutUser()
-//                }
-//                
-//            })
-//
-//    }
+    
+    func finishSignUp(){
+        print("nil")
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "CompleteSignUp")
+        self.present(vc!, animated: true, completion: {
+            let alert = UIAlertController(title: "Please complete sign up", message: "You're one step away from the app!", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Okay!", style: UIAlertActionStyle.default, handler: nil))
+            
+            vc?.present(alert, animated: true) {
+                
+                print(self.user?.email)
+            }
+        })
+    }
+    
+    //    func fetchUserData(){
+    //        let currentUserRef = ref.child("users").queryOrdered(byChild: "email")
+    //            .queryEqual(toValue: user?.email).observeSingleEvent(of: .value, with: { (snapshot) in
+    //
+    //                if let value = snapshot.value as? NSDictionary{
+    //                    var hasVid = false
+    //                    //GOT USER OBJECT
+    //                    let thisUserObject = value.allValues.first as! NSDictionary
+    //
+    //                    for actualProperties in thisUserObject as NSDictionary{
+    //                        if let keyTitle = actualProperties.key as? String {
+    //
+    //                            if keyTitle == "full_name"{
+    //                                if let text = actualProperties.value as? String {
+    //                                    self.jobsLabel.text = text
+    //                                }
+    //                            }
+    //                            var customer = false
+    //
+    //
+    //
+    //
+    //                            if keyTitle == "home_type"{
+    //                                if let text = actualProperties.value as? String {
+    //                                    self.homeTypeLabel.text = text
+    //                                }
+    //                            }
+    //
+    //                            if keyTitle == "phone_number"{
+    //                                if let text = actualProperties.value as? String {
+    //                                    self.phoneNumberLabel.text = text
+    //                                }
+    //                            }
+    //
+    //
+    //                            if keyTitle == "full_name"{
+    //                                if let text = actualProperties.value as? String {
+    //                                    self.jobsLabel.text = text
+    //                                }
+    //                            }
+    //
+    //                        }
+    //                    }
+    //
+    //
+    //
+    //
+    //                }
+    //
+    //            })
+    //
+    //    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func addItemToDB(_ sender: Any) {
         
         self.ref.child("items").childByAutoId().setValue(["text": textField.text])
         
     }
-
+    
     @IBAction func logoutUser(_ sender: Any) {
         logoutUser()
         
@@ -216,9 +235,9 @@ class FirstViewController: UIViewController {
     }
     func logoutUser() {
         do{
-        UserDefaults.standard.setValue(nil, forKey: "uid")
+            UserDefaults.standard.setValue(nil, forKey: "uid")
         }catch {
-        print("error logging out")
+            print("error logging out")
         }
         
         let loginViewController = self.storyboard!.instantiateViewController(withIdentifier: "Login")
